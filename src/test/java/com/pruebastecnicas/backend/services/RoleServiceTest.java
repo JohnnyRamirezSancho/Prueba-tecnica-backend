42ca22f3bc8ff1677d08289b2da80b23f4cbb984
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-03-29 09:24:07 +0200

    End test role service

--- src/test/java/com/pruebastecnicas/backend/services/RoleServiceTest.java
+++ src/test/java/com/pruebastecnicas/backend/services/RoleServiceTest.java
@@ -2,9 +2,11 @@ package com.pruebastecnicas.backend.services;
 
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,38 +29,35 @@ public class RoleServiceTest {
     @Test
     void testGetAll() {
         Role roleUser = new Role(null, "ROLE_USER");
+        Role roleAdmin = new Role(null, "ROLE_ADMIN");
  
         List<Role> roles = new ArrayList<>();
  
         roles.add(roleUser);
+        roles.add(roleAdmin);
  
         when(repository.findAll()).thenReturn(roles);
  
         List<Role> reponseRolesTest = service.getAll();
  
-        assertThat(reponseRolesTest.size()).isEqualTo(1);
+        assertThat(reponseRolesTest.size()).isEqualTo(2);
         assertThat(reponseRolesTest.get(0).getRole()).isEqualTo("ROLE_USER");
      }
 
     @Test
     void testGetOne() {
-        Role roleUser = new Role(null, "ROLE_USER");
-
-        when(repository.findByRole("ROLE_USER").isPresent());
-        
-        service.getOneByRole("ROLE_USER");
-
+        Role roleUser = new Role((long) 1, "ROLE_USER");
+        when(repository.findById((long) 1)).thenReturn(Optional.of(roleUser));
+        service.getOne((long) 1);
         assertThat(roleUser.getRole()).isEqualTo("ROLE_USER");
     }
 
     @Test
-    void testSave() {
-
-    }
-
-    @Test
     void testDelete() {
-
+        Role roleNew = new Role((long) 1, "ROLE_ADMIN");
+        repository.save(roleNew);
+        repository.deleteById(roleNew.getId_role());
+        Optional<Role> optional = repository.findById((long) 1);
+        assertEquals(Optional.empty(), optional);
     }    
-
 }
